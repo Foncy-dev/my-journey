373b9ea24b3a970efbccb80d3dbd169955f1775d
Foncy-dev <devikwozaaa@gmail.com>
2025-02-17 11:59:01 +0300

    removing the comments

--- OOP/ooptest.py
+++ OOP/ooptest.py
@@ -1,73 +1,73 @@
-#  OOP learning tst
+ OOP learning tst
 
-#     Encapsulation  concepts
+    Encapsulation  concepts
 
-# class BankAccount:
-#     def __init__ (self,owner,balance):
-#         self.owner = owner
-#         self.balance = balance
+class BankAccount:
+    def __init__ (self,owner,balance):
+        self.owner = owner
+        self.balance = balance
     
-#     def deposit(self,amount):
-#         self.balance += amount
+    def deposit(self,amount):
+        self.balance += amount
         
-#     def withdraw(self,amount):
-#         if self.balance >= amount:
-#             self.balance -= amount
-#         else:
-#             print("insufficient funds")
+    def withdraw(self,amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print("insufficient funds")
 
-#     def get_balance(self):
-#         return self.balance
+    def get_balance(self):
+        return self.balance
     
-#     def __str__ (self):
-#         return f"Account owner: {self.owner}\nAccount balance: {self.balance}"
+    def __str__ (self):
+        return f"Account owner: {self.owner}\nAccount balance: {self.balance}"
     
 
-# account = BankAccount("foncy", 10000)
-# account.withdraw(9000000002)
-# print(account.owner)
-# print(account.get_balance())
+account = BankAccount("foncy", 10000)
+account.withdraw(9000000002)
+print(account.owner)
+print(account.get_balance())
 
 
 
 
 
-#   Inheritance concepts
+  Inheritance concepts
 
-# class Animals:
-#     def __init__ (self,name):
-#         self.name = name
-#     def make_sound(self):
-#         return  "some sound"
+class Animals:
+    def __init__ (self,name):
+        self.name = name
+    def make_sound(self):
+        return  "some sound"
 
-# class Dog(Animals):
-#     def make_sound(self):
-#         return "Bark"
+class Dog(Animals):
+    def make_sound(self):
+        return "Bark"
     
-# class Cat(Animals):
-#     def make_sound(self):
-#         return "Meow"
+class Cat(Animals):
+    def make_sound(self):
+        return "Meow"
     
-# dog = Dog("Rex")   
-# cat = Cat("kitty") 
-# print(dog.name)
-# print(dog.make_sound())
-# print(cat.name)
-# print(cat.make_sound())
+dog = Dog("Rex")   
+cat = Cat("kitty") 
+print(dog.name)
+print(dog.make_sound())
+print(cat.name)
+print(cat.make_sound())
 
 
-#    Polymorphism concepts
+   Polymorphism concepts
 
-# class Birds:
-#     def sound(self):
-#         return "chirp"
+class Birds:
+    def sound(self):
+        return "chirp"
     
-# class Cat:
-#     def sound(self):
-#         return "Meow"
+class Cat:
+    def sound(self):
+        return "Meow"
     
-# for animals in (Birds(), Cat()):
-#     print(animals.sound())
+for animals in (Birds(), Cat()):
+    print(animals.sound())
 
 
 #   Abstraction concepts
